26fd2def510f75a951aceaadddeb84054d6125bb
José Ángel Ramos Martín <joseangelramos16@gmail.com>
2026-07-31 01:49:54 +0200

    imports eliminados

--- backend/stay-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/config/RabbitMQConfig.java
+++ backend/stay-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/config/RabbitMQConfig.java
@@ -1,8 +1,5 @@
 package com.tartis_recon_ai_parking.infrastructure.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
